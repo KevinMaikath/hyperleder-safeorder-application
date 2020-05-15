dfdc568da624e8eb02ad679265c57acab748f929
Kevin <kevinrx25@gmail.com>
2020-05-15 12:28:16 +0100

    Database set-up

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {categoriesEnum, ProductService} from "../../services/product.service";
+import {ProductModel} from "../../models/product.model";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-main',
@@ -10,11 +13,15 @@ export class MainComponent implements OnInit {
   leftMenuOpen = false;
   rightMenuOpen = false;
 
+  product: ProductModel;
+  ingredients: string;
 
-  constructor() {
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit(): void {
+    this.product = new ProductModel();
+    this.ingredients = '';
   }
 
   get middleWidth() {
@@ -38,5 +45,20 @@ export class MainComponent implements OnInit {
   toggleRightMenu() {
     this.rightMenuOpen = !this.rightMenuOpen;
   }
+
+  async onConsole() {
+    const result = await this.productService.getProductsFromCategory(categoriesEnum.HAMBURGERS).pipe(take(1)).toPromise();
+    console.log(result);
+  }
+
+  // setIngredients() {
+  //   this.product.ingredients = this.ingredients.split(',');
+  // }
+
+  // async onPush() {
+  //   this.setIngredients();
+  //   this.product.ID = await this.productService.generatePushID();
+  //   await this.productService.pushProduct(this.product);
+  // }
 
 }
